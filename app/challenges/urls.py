9e97739ef38d976de1a60c3893d74807cc916abf
Tejas Kale <99819740+tejas-kale-relayr@users.noreply.github.com>
2022-09-19 12:45:15 +0200

    Adds examples of URLs and Routes (#1)
    * [T-iWrG] Adds examples of `reverse` and dynamic views
    
    Things 3 URL: things:///show?id=5R2aiKFWi8aCntqYppiWrG
    
    * [T-iWrG] Checks if `tejas-kale` is granted access to `kaletejas2006` organisation's repositories
    
    Things 3 URL: things:///show?id=5R2aiKFWi8aCntqYppiWrG

--- app/challenges/urls.py
+++ app/challenges/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from challenges import views
 
 urlpatterns: list = [
+    path("", views.index_page),
     path("hello", views.hello_django),
     path("january", views.january),
     path("<int:month>", views.monthly_challenge_numeric),
-    path("<str:month>", views.monthly_challenge)
+    path("<str:month>", views.monthly_challenge, name="month-name")
 ]
--- app/challenges/views.py
+++ app/challenges/views.py
@@ -1,8 +1,8 @@
-from typing import Optional
+from typing import Optional, Union
 
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
-
+from django.urls import reverse
 
 # Create your views here.
 
@@ -12,7 +12,7 @@ monthly_challenges: dict = {
 }
 
 
-def hello_django(req: WSGIRequest):
+def hello_django(req: WSGIRequest) -> HttpResponse:
     """
     Hello world program from Django. Any Django view function always
     gets an input of type `WSGIRequest`.
@@ -20,26 +20,54 @@ def hello_django(req: WSGIRequest):
     return HttpResponse(f"Hello Django from {req.path}!")
 
 
-def january(req: Optional[WSGIRequest]):
+def january(req: Optional[WSGIRequest]) -> HttpResponse:
     """Define a challenge for January."""
     return HttpResponse("Eat no meat for the entire month!")
 
 
-def monthly_challenge_numeric(req: Optional[WSGIRequest], month: int):
-    supported_months: list = list(monthly_challenges.keys())
+def get_supported_months() -> list:
+    return list(monthly_challenges.keys())
+
+
+def monthly_challenge_numeric(
+        req: Optional[WSGIRequest],
+        month: int
+) -> Union[HttpResponseRedirect, HttpResponseNotFound]:
+    supported_months: list = get_supported_months()
 
     if not (2 <= month <= 3):
-        return HttpResponseNotFound("This month is not yet supported with numeric index!")
+        return HttpResponseNotFound("<h3>This month is not yet supported with numeric index!</h3>")
 
     redirect_month: str = supported_months[month - 2]
-    return HttpResponseRedirect(f"/challenges/{redirect_month}")
+    return HttpResponseRedirect(reverse("month-name", args=[redirect_month]))
 
 
-def monthly_challenge(req: Optional[WSGIRequest], month: str):
+def monthly_challenge(
+        req: Optional[WSGIRequest],
+        month: str
+) -> Union[HttpResponse, HttpResponseNotFound]:
     """Define month-wise challenges."""
     try:
         selected_challenge: str = monthly_challenges[month]
+        response: str = f"<h3>{selected_challenge}</h3>"
     except KeyError:
-        return HttpResponseNotFound("This month is not yet supported!")
+        return HttpResponseNotFound("<h3>This month is not yet supported!</h3>")
 
-    return HttpResponse(selected_challenge)
+    return HttpResponse(response)
+
+
+def index_page(req: Optional[WSGIRequest]) -> HttpResponse:
+    """Index page with links to February and March challenges."""
+    supported_months: list = get_supported_months()
+
+    month_list: str = ""
+    month: str
+    for month in supported_months:
+        month_url: str = reverse("month-name", args=[month])
+        month_list_item: str = (f'<li><a href="{month_url}">'
+                                f'{month.capitalize()}</a></li>')
+        month_list += month_list_item
+
+    response: str = f"<ul>{month_list}</ul>"
+
+    return HttpResponse(response)
