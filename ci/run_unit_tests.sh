da1ace24963cd8913410b57ac4bda896a12e6a99
tejas-kale-relayr <99819740+tejas-kale-relayr@users.noreply.github.com>
2022-09-29 15:49:59 +0200

    [T-5UC8] Corrects path to requirements.txt
    Things 3 URL: things:///show?id=GxFqgaNhYgGecR4KK25UC8

--- ci/run_unit_tests.sh
+++ ci/run_unit_tests.sh
@@ -2,7 +2,7 @@ PYTHON_VERSION="$(python --version)"
 echo "Python version: $PYTHON_VERSION"
 
 echo "Installing packages from 'requirements.txt'"
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 
 echo "Executing tests for the 'challenges' app."
 python manage.py test challenges
