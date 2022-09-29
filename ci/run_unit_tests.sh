a092b7f4776466f12292e4e5f3624a7c21ca4cb6
tejas-kale-relayr <99819740+tejas-kale-relayr@users.noreply.github.com>
2022-09-29 15:47:50 +0200

    [T-5UC8] Extends Bash script to execute unit tests for challenges app
    Things 3 URL: things:///show?id=GxFqgaNhYgGecR4KK25UC8

--- ci/run_unit_tests.sh
+++ ci/run_unit_tests.sh
@@ -1,2 +1,8 @@
 PYTHON_VERSION="$(python --version)"
-echo "$PYTHON_VERSION"
+echo "Python version: $PYTHON_VERSION"
+
+echo "Installing packages from 'requirements.txt'"
+pip install -r requirements.txt
+
+echo "Executing tests for the 'challenges' app."
+python manage.py test challenges
