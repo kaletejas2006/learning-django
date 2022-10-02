72a79e9de8387b702a1e804c0ff794d443fc214a
Tejas Kale <kaletejas2006@gmail.com>
2022-10-02 19:56:57 +0200

    [T-5T6J] Adds templates and static files
    Things 3 URL: things:///show?id=Fii9nqTBTWJRW9NoRu5T6J
    
    Includes learnings about:
    - Creating and using templates
    - Creating and using partial templates
    - Including static files like CSS stylesheets

--- app/challenges/urls.py
+++ app/challenges/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from challenges import views
 
 urlpatterns: list = [
-    path("", views.index_page),
+    path("", views.index_page, name="index"),
     path("hello", views.hello_django),
     path("january", views.january),
     path("<int:month>", views.monthly_challenge_numeric),
-    path("<str:month>", views.monthly_challenge, name="month-name")
+    path("<str:month>", views.monthly_challenge, name="month-name"),
 ]
--- app/challenges/views.py
+++ app/challenges/views.py
@@ -1,14 +1,21 @@
 from typing import Optional, Union
 
 from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import (
+    HttpResponse,
+    HttpResponseNotFound,
+    HttpResponseRedirect,
+    Http404,
+)
+from django.shortcuts import render
 from django.urls import reverse
 
 # Create your views here.
 
 monthly_challenges: dict = {
     "february": "Drink at least 2 litres of water every day!",
-    "march": "Practice Django for at least 20 minutes every day!"
+    "march": "Practice Django for at least 20 minutes every day!",
+    "april": None,
 }
 
 
@@ -30,44 +37,38 @@ def get_supported_months() -> list:
 
 
 def monthly_challenge_numeric(
-        req: Optional[WSGIRequest],
-        month: int
+    req: Optional[WSGIRequest], month: int
 ) -> Union[HttpResponseRedirect, HttpResponseNotFound]:
     supported_months: list = get_supported_months()
 
     if not (2 <= month <= 3):
-        return HttpResponseNotFound("<h3>This month is not yet supported with numeric index!</h3>")
+        return HttpResponseNotFound(
+            "<h3>This month is not yet supported with numeric index!</h3>"
+        )
 
     redirect_month: str = supported_months[month - 2]
     return HttpResponseRedirect(reverse("month-name", args=[redirect_month]))
 
 
 def monthly_challenge(
-        req: Optional[WSGIRequest],
-        month: str
-) -> Union[HttpResponse, HttpResponseNotFound]:
+    req: Optional[WSGIRequest], month: str
+) -> Union[HttpResponse, Http404]:
     """Define month-wise challenges."""
     try:
         selected_challenge: str = monthly_challenges[month]
-        response: str = f"<h3>{selected_challenge}</h3>"
+        return render(
+            req,
+            "challenges/challenge.html",
+            {"month": month, "text": selected_challenge},
+        )
     except KeyError:
-        return HttpResponseNotFound("<h3>This month is not yet supported!</h3>")
-
-    return HttpResponse(response)
+        raise Http404()
 
 
 def index_page(req: Optional[WSGIRequest]) -> HttpResponse:
     """Index page with links to February and March challenges."""
     supported_months: list = get_supported_months()
 
-    month_list: str = ""
-    month: str
-    for month in supported_months:
-        month_url: str = reverse("month-name", args=[month])
-        month_list_item: str = (f'<li><a href="{month_url}">'
-                                f'{month.capitalize()}</a></li>')
-        month_list += month_list_item
-
-    response: str = f"<ul>{month_list}</ul>"
+    response: str = render(req, "challenges/index.html", {"months": supported_months})
 
     return HttpResponse(response)
