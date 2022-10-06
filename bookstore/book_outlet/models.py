e8c4ebc0ee53106453e3fe159555b8a8770b3b8b
tejas-kale-relayr <99819740+tejas-kale-relayr@users.noreply.github.com>
2022-10-06 18:54:37 +0200

    [T-BzU9] Adds test to insert and check data and modifies models
    Things 3 URL: things:///show?id=89friVqV4CrXe6nDXKBzU9

--- bookstore/book_outlet/models.py
+++ bookstore/book_outlet/models.py
@@ -1,7 +1,22 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    rating = models.IntegerField()
+    author = models.CharField(default="Not Provided", max_length=50)
+    rating = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+    is_nyt_bestseller = models.BooleanField(default=False)
+
+    def __str__(self):
+        print_title: str = f"{self.title} by {self.author}"
+        if self.is_nyt_bestseller:
+            print_title = f"{print_title} [NY Times Bestseller]"
+
+        return print_title
